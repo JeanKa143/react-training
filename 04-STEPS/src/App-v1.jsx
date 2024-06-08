f337a5049ecfd7e79ba16e7ce140723194b34936
Jean Carlo Jimenez <jeanjimenez143@outlook.com>
2024-06-08 16:05:54 -0600

    04-STEPS - Children prop

--- 04-STEPS/src/App-v1.jsx
+++ 04-STEPS/src/App-v1.jsx
@@ -1,17 +1,8 @@
-/* eslint-disable react/prop-types */
 import { useState } from 'react'
 
 const messages = ['Learn React ⚛️', 'Apply for jobs 💼', 'Invest your new income 🤑']
 
 function App() {
-  return (
-    <div>
-      <Steps />
-    </div>
-  )
-}
-
-function Steps() {
   const [step, setStep] = useState(1)
   const [isOpen, setIsOpen] = useState(true)
 
@@ -36,36 +27,21 @@ function Steps() {
             <div className={step >= 3 ? 'active' : ''}>3</div>
           </div>
 
-          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
 
           <div className="buttons">
-            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
-              <span>👈</span> Previous
-            </Button>
-            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
-              Next <span>👉</span>
-            </Button>
+            <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePrevious}>
+              Previous
+            </button>
+            <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handleNext}>
+              Next
+            </button>
           </div>
         </div>
       )}
     </>
-  )
-}
-
-function StepMessage({ step, children }) {
-  return (
-    <div className="message">
-      <h3>Step {step}</h3>
-      {children}
-    </div>
-  )
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button style={{ backgroundColor: bgColor, color: textColor }} onClick={onClick}>
-      {children}
-    </button>
   )
 }
 
